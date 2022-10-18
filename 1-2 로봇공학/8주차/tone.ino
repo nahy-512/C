08d401d8581e5cc42991f06939cbc4caa28a7ea4
nahy-512 <hedghog1004@naver.com>
2022-10-18 22:54:00 +0900

    로공 8주차 수업

---
+++ 1-2 로봇공학/8주차/tone.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(9,262,500);
+  delay(500);
+  noTone(9);
+}
